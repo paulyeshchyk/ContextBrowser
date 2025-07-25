9adbf405d1674e3aa1a6727b50c1c7ec265afe08
cts\eshchik <eshchik_pm@ascon.by>
2025-07-25 13:48:12 +0300

    cleanup

--- DiagramFactory/Builders/ContextTransitionDiagramBuilder.cs
+++ DiagramFactory/Builders/ContextTransitionDiagramBuilder.cs
@@ -17,35 +17,23 @@ public class ContextTransitionDiagramBuilder : IContextDiagramBuilder
                 classifier.HasActionAndDomain(ctx))
             .ToList();
 
-        if (!filtered.Any())
+        if(!filtered.Any())
             return false;
-
-        //onGetDescendants: method =>
-        //{
-        //    //var refs = method.References ?? Enumerable.Empty<ContextInfo>();
-        //    //return refs.Join(allContexts,
-        //    //                 r => r.FullName,
-        //    //                 a => a.FullName,
-        //    //                 (r, a) => a); // ← возвращаем объект из allContexts
-        //},
-
-        //            onGetDescendants: m => m.References?.Where(r => r.ElementType == ContextInfoElementType.method) ?? Enumerable.Empty<ContextInfo>(),
 
         var walker = new ItemWalker(
             onGetDescendants: method =>
             {
-                //var reverse = allContexts.SelectMany(c => c.References).Where(r => r.DisplayName == method.DisplayName).Distinct().ToList();
-                return method.References ?? Enumerable.Empty<ContextInfo>();//?? reverse 
+                return method.References ?? Enumerable.Empty<ContextInfo>();
             },
             onGetDomainItems: d =>
             {
                 return allContexts.Where(c => c.Contexts.Contains(d) && classifier.HasActionAndDomain(c));
             },
-            onExportItem: (caller, callee, calleeDomainItem, domain) =>
+            onExportItem:(caller, callee, calleeDomainItem, domain) =>
             {
-                var callerLabel = $"{caller.ClassOwner?.Name}.{caller.Name}";//.Replace(".", "_");
-                var calleeLabel = $"{callee.ClassOwner?.Name}.{callee.Name}";//.Replace(".", "_");
-                var domainLabel = domain;//.Replace(".", "_");
+                var callerLabel = $"{caller.ClassOwner?.Name}.{caller.Name}";
+                var calleeLabel = $"{callee.ClassOwner?.Name}.{callee.Name}";
+                var domainLabel = domain;
 
                 target.AddParticipant(domainLabel);
                 target.AddParticipant(callerLabel);
