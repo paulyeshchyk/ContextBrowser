d3be080222db39a861a935176bf3e908af84287a
cts\eshchik <eshchik_pm@ascon.by>
2025-07-26 20:45:01 +0300

    added Tests sample

---
+++ ContextBrowserTests/Test1.cs
@@ -0,0 +1,19 @@
+﻿namespace ContextBrowserTests;
+
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    [DataRow("// context: Test1", "Test1")]
+    public void TestParseSingle(string comment, string context)
+    {
+    }
+
+    [TestMethod]
+    [DataRow("// context: Test1, Test2", "Test1")]
+    [DataRow("// context: Test1, Test2", "Test2")]
+    public void TestParseDouble(string comment, string context)
+    {
+    }
+}
