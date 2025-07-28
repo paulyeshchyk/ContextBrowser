c6398d12c5f417d9f8e8900e966b3c270d6c624d
cts\eshchik <eshchik_pm@ascon.by>
2025-07-29 00:30:52 +0300

    app options

--- ContextBrowser/Program.cs
+++ ContextBrowser/Program.cs
@@ -16,85 +16,170 @@ namespace ContextBrowser.ContextCommentsParser;
 // context: app, model
 public static class Program
 {
-    // context: app, file, delete
+    // context: app, execute
     public static void Main(string[] args)
     {
-#warning revert path \\Samples\\Test
-        string theSourcePath = ".\\..\\..\\..\\..\\ContextBrowser";
-        string outputDirectory = ".\\output\\";
+        var options = new AppOptions();
 
-        bool includeAllStandardActions = true;
-        var summaryPlacement = SummaryPlacement.AfterFirst;
-        var matrixOrientation = MatrixOrientation.DomainRows;
-        var unclassifiedPriority = UnclassifiedPriority.Highest;
+        DirectorePreparator.Prepare(options);
 
-        var contextsList = RoslynContextParser.Parse(theSourcePath, new List<string>() { outputDirectory });
+        var contextBuilderModel = ContextModelBuildBuilder.Build(options);
 
-        var contextClassifier = new ContextClassifier();
-        var matrix = ContextMatrixUmlExporter.GenerateMatrix(contextsList, contextClassifier, unclassifiedPriority, includeAllStandardActions);
+        ExtraDiagramsBuilder.Build(contextBuilderModel, options);
 
-        var contextLookup = contextsList
+        ComponentDiagram.Build(contextBuilderModel, options);
+
+        ActionPerDomainDiagramBuilder.Build(contextBuilderModel, options);
+
+        ContentHtmlBuilder.Build(contextBuilderModel, options);
+
+        IndexHtmlBuilder.Build(contextBuilderModel, options);
+
+        DimensionBuilder.Build(contextBuilderModel, options);
+    }
+}
+
+public class AppOptions
+{
+    public string theSourcePath = ".\\..\\..\\..\\..\\ContextBrowser\\Samples\\";
+    public string outputDirectory = ".\\output\\";
+    public UnclassifiedPriority unclassifiedPriority = UnclassifiedPriority.Highest;
+    public bool includeAllStandardActions = true;
+    public SummaryPlacement summaryPlacement = SummaryPlacement.AfterFirst;
+    public MatrixOrientation matrixOrientation = MatrixOrientation.DomainRows;
+}
+
+public static class DirectorePreparator
+{
+    public static void Prepare(AppOptions options)
+    {
+        FileUtils.CreateDirectoryIfNotExists(options.outputDirectory);
+        FileUtils.WipeDirectory(options.outputDirectory);
+    }
+}
+
+public class ContextBuilderModel
+{
+    public List<ContextInfo> contextsList;
+    public Dictionary<ContextContainer, List<string>> matrix;
+    public Dictionary<string, ContextInfo> contextLookup;
+
+    public ContextBuilderModel(List<ContextInfo> contextsList, Dictionary<ContextContainer, List<string>> matrix, Dictionary<string, ContextInfo> contextLookup)
+    {
+        this.contextsList = contextsList;
+        this.matrix = matrix;
+        this.contextLookup = contextLookup;
+    }
+}
+
+public static class ExtraDiagramsBuilder
+{
+    public static void Build(ContextBuilderModel model, AppOptions options)
+    {
+        //ContextMatrixUmlExporter.GenerateCsv(matrix, $"{theOutputPath}matrix.csv");
+        //HeatmapExporter.GenerateHeatmapCsv(matrix, $"{theOutputPath}heatmap.csv", unclassifiedPriority);
+
+
+        UmlContextPackagesDiagram.Build(model.contextsList, $"{options.outputDirectory}uml.packages.domains.puml");
+        UmlContextMethodsOnlyDiagram.Build(model.contextsList, $"{options.outputDirectory}methodlinks.puml");
+        UmlContextMethodPerActionDomainDiagram.Build(model.matrix, $"{options.outputDirectory}uml.packages.actions.puml");
+
+        var links = ContextMatrixUmlExporter.GenerateMethodLinks(model.contextsList, new ContextClassifier());
+        UmlContextRelationDiagram.GenerateLinksUml(links, $"{options.outputDirectory}uml.4.links.puml");
+    }
+}
+
+public static class ContextModelBuildBuilder
+{
+    public static ContextBuilderModel Build(AppOptions options)
+    {
+        var contextsList = RoslynContextParser.Parse(options.theSourcePath, new List<string>() { options.outputDirectory });
+
+        var matrix = ContextMatrixUmlExporter.GenerateMatrix(contextsList, new ContextClassifier(), options.unclassifiedPriority, options.includeAllStandardActions);
+
+        var contextLookup = GenerateContextLookup(contextsList);
+        return new ContextBuilderModel(contextsList, matrix, contextLookup);
+    }
+
+    private static Dictionary<string, ContextInfo> GenerateContextLookup(List<ContextInfo> contextsList)
+    {
+        return contextsList
             .Where(c => !string.IsNullOrWhiteSpace(c.Name))
             .GroupBy(c => c.ElementType == ContextInfoElementType.method && !string.IsNullOrWhiteSpace(c.ClassOwner?.Name)
                 ? $"{c.ClassOwner?.Name}.{c.Name}"
                 : $"{c.Name}")
             .ToDictionary(g => g.Key, g => g.First());
+    }
+}
 
-        FileUtils.WipeDirectory(outputDirectory);
-        FileUtils.CreateDirectoryIfNotExists(outputDirectory);
-
-
-        //ContextMatrixUmlExporter.GenerateCsv(matrix, $"{theOutputPath}matrix.csv");
-        //HeatmapExporter.GenerateHeatmapCsv(matrix, $"{theOutputPath}heatmap.csv", unclassifiedPriority);
-
-
-        UmlContextPackagesDiagram.Build(contextsList, $"{outputDirectory}uml.packages.domains.puml");
-        UmlContextMethodsOnlyDiagram.Build(contextsList, $"{outputDirectory}methodlinks.puml");
-        UmlContextMethodPerActionDomainDiagram.Build(matrix, $"{outputDirectory}uml.packages.actions.puml");
-
-        var links = ContextMatrixUmlExporter.GenerateMethodLinks(contextsList, contextClassifier);
-        UmlContextRelationDiagram.GenerateLinksUml(links, $"{outputDirectory}uml.4.links.puml");
-
-
-        // 1.
-        UmlContextComponentDiagram.Build(matrix, outputDirectory);
-
-        // 2.
-        UmlContextActionPerDomainDiagram.Build(matrix,(action, domain) => $"composite_{action}_{domain}.puml", $"{outputDirectory}uml.heatmap.link.puml");
-
-        // 3.
-        HtmlIndexPage.GenerateContextHtmlPages(matrix, outputDirectory);
-
-        // 4.
+public static class IndexHtmlBuilder
+{
+    // context: step3, build
+    public static void Build(ContextBuilderModel model, AppOptions options)
+    {
         IndexGenerator.GenerateContextIndexHtml(
-            matrix,
-            contextLookup,
-            outputFile: $"{outputDirectory}index.html",
-            priority: unclassifiedPriority,
-            orientation: matrixOrientation,
-            summaryPlacement: summaryPlacement
+            model.matrix,
+            model.contextLookup,
+            outputFile: $"{options.outputDirectory}index.html",
+            priority: options.unclassifiedPriority,
+            orientation: options.matrixOrientation,
+            summaryPlacement: options.summaryPlacement
             );
+    }
+}
 
+// context: step3, build
+public static class ContentHtmlBuilder
+{
+    // context: step3, build
+    public static void Build(ContextBuilderModel model, AppOptions options)
+    {
+        HtmlIndexPage.GenerateContextHtmlPages(model.matrix, options.outputDirectory);
+    }
+}
 
+public static class ComponentDiagram
+{
+    public static void Build(ContextBuilderModel model, AppOptions options)
+    {
+        UmlContextComponentDiagram.Build(model.matrix, options.outputDirectory);
+    }
+}
+
+// context: step2, build
+public static class ActionPerDomainDiagramBuilder
+{
+    public static void Build(ContextBuilderModel model, AppOptions options)
+    {
+        UmlContextActionPerDomainDiagram.Build(model.matrix,(action, domain) => $"composite_{action}_{domain}.puml", $"{options.outputDirectory}uml.heatmap.link.puml");
+    }
+}
+
+// context: step5, build
+public static class DimensionBuilder
+{
+    // context: step5, build
+    public static void Build(ContextBuilderModel model, AppOptions options)
+    {
         var callback = AdaptToDomainCallback(
-            contextItems: contextsList,
-            classifier: contextClassifier,
-            outputPath: outputDirectory,
+            contextItems: model.contextsList,
+            classifier: new ContextClassifier(),
+            outputPath: options.outputDirectory,
             factory:() => ContextDiagramFactory.Get("context-transition")!
         );
 
-        HtmlContextDimensionBuilder builder = new(
-            matrix,
-            contextsList,
-            outputDirectory,
+        var builder = new HtmlContextDimensionBuilder(
+            model.matrix,
+            model.contextsList,
+            options.outputDirectory,
             callback,
             () => ContextDiagramFactory.Transition);
 
         builder.Build();
     }
 
-
-    public static Func<string, bool> AdaptToDomainCallback(List<ContextInfo> contextItems, ContextClassifier classifier, string outputPath, Func<IContextDiagramBuilder> factory)
+    // context: step5, build
+    internal static Func<string, bool> AdaptToDomainCallback(List<ContextInfo> contextItems, ContextClassifier classifier, string outputPath, Func<IContextDiagramBuilder> factory)
     {
         return domain =>
         {
